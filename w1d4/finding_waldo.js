64c0b37a9d5e6e26a4219a212a7e7397300e39a4
Paulyn <paulyntran@live.ca>
2019-02-07 17:57:26 +0000

    Updating finding waldo with forEach

--- w1d4/finding_waldo.js
+++ w1d4/finding_waldo.js
@@ -1,17 +1,34 @@
 // The second argument/parameter is expected to be a function
 var holder = ""
+var index = ""
 
 function findWaldo(arr, found) {
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i] === "Waldo") {
-        holder = i + 1;
-        found();   // execute callback
-      }
-    }
-  }
+    // for (var i = 0; i < arr.length; i++) {
+    //   if (arr[i] === "Waldo") {
+    //     holder = i;
+    //     found();   // execute callback
+    //   }
+    // }
+    var list = arr
+    list.forEach(function(x) {
+        if (x === "Waldo") {
+            index = list.findIndex(waldo => waldo === "Waldo");
+            found ();
+        }
+    });
+  }  
+
+function actionWhenFound() {
+   console.log("Found Waldo in index " + index + "!");
+}
   
-  function actionWhenFound() {
-    console.log("Found Waldo in index " + holder + "!");
-  }
-  
-  findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+// var list = [1, 2, 3, 4, 5];
+
+// list.forEach(function(x) {
+//     console.log(x);
+// });
+
